c011e7b94e9ab863f11e55ad5241139fa2e2bce6
Guillaume Quintard <guillaume.quintard@gmail.com>
2021-12-24 17:20:15 -0800

    VDP replacer, yay!

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 varnish::boilerplate!();
 
+use std::borrow::Cow;
+use std::os::raw::c_void;
 use std::slice;
 use std::str::{from_utf8, from_utf8_unchecked};
 use std::sync::Mutex;
 
 use varnish::vcl::convert::IntoVCL;
-use varnish::vcl::ctx::{Ctx, LogTag};
+use varnish::vcl::ctx::{Ctx, Event, LogTag, TestCtx};
+use varnish::vcl::processor::{new_vdp, OutAction, OutCtx, OutProc, OutResult};
 use varnish::vcl::vpriv::VPriv;
 use varnish_sys::VCL_STRING;
 
@@ -14,10 +17,17 @@ varnish::vtc!(test02);
 varnish::vtc!(test03);
 varnish::vtc!(test04);
 varnish::vtc!(test05);
+varnish::vtc!(test06);
 
 #[allow(non_camel_case_types)]
 pub struct init {
     mutexed_cache: Mutex<regex_cache::RegexCache>,
+}
+
+const PRIV_ANCHOR: [u8; 1] = [0];
+
+struct ReplaceSteps {
+    v: Vec<(regex_cache::Regex, String)>,
 }
 
 pub struct Captures<'a> {
@@ -73,7 +83,7 @@ impl init {
         }
     }
 
-    pub fn capture_body<'a>(
+    pub fn capture_req_body<'a>(
         &self,
         ctx: &mut Ctx,
         vp: &mut VPriv<Captures<'a>>,
@@ -152,10 +162,133 @@ impl init {
 
     pub fn group<'a>(&self, _: &mut Ctx, vp: &mut VPriv<Captures<'a>>, n: i64) -> &'a str {
         let n = if n >= 0 { n } else { 0 } as usize;
-        vp.as_ref().and_then(|c| c.caps.get(n)).map(|m| m.as_str()).unwrap_or("")
+        vp.as_ref()
+            .and_then(|c| c.caps.get(n))
+            .map(|m| m.as_str())
+            .unwrap_or("")
     }
 
-    pub fn named_group<'a>(&self, _: &mut Ctx, vp: &mut VPriv<Captures<'a>>, name: &str) -> &'a str {
-        vp.as_ref().and_then(|c| c.caps.name(name)).map(|m| m.as_str()).unwrap_or("")
+    pub fn named_group<'a>(
+        &self,
+        _: &mut Ctx,
+        vp: &mut VPriv<Captures<'a>>,
+        name: &str,
+    ) -> &'a str {
+        vp.as_ref()
+            .and_then(|c| c.caps.name(name))
+            .map(|m| m.as_str())
+            .unwrap_or("")
     }
+
+    pub fn replace_resp_body(&self, ctx: &mut Ctx, res: &str, sub: &str) {
+        let re = match self.get_regex(res) {
+            Err(s) => {
+                ctx.log(LogTag::VclError, &s);
+                return ();
+            }
+            Ok(re) => re,
+        };
+        //TODO: check for VRT_priv_task failure
+        let mut vp: VPriv<ReplaceSteps> = unsafe {
+            VPriv::new(varnish_sys::VRT_priv_task(
+                ctx.raw,
+                PRIV_ANCHOR.as_ptr() as *const c_void,
+            ))
+        };
+        if let Some(ri) = vp.as_mut() {
+            ri.v.push((re, sub.to_owned()));
+        } else {
+            let ri = ReplaceSteps {
+                v: vec![(re, sub.to_owned())],
+            };
+            vp.store(ri);
+        }
+    }
+}
+
+#[derive(Default)]
+struct DeliveryReplacer {
+    steps: Vec<(regex_cache::Regex, String)>,
+    body: Vec<u8>,
+}
+
+impl OutProc for DeliveryReplacer {
+    fn new(ctx: &mut OutCtx, _oc: *mut varnish_sys::objcore) -> Result<Self, String> {
+        let priv_opt;
+        // we don't know how/if the body will be modified, so we nuke the content-length
+        // it's also no worth fleshing out a rust object just to remove a header, use the C function
+        unsafe {
+            let req = ctx.raw.req.as_ref().unwrap();
+            assert_eq!(req.magic, varnish_sys::REQ_MAGIC);
+            varnish_sys::http_Unset((*ctx.raw.req).resp, varnish_sys::H_Content_Length.as_ptr());
+
+            // the lying! the cheating!
+            let mut fake_ctx = TestCtx::new(0);
+            fake_ctx.ctx().raw.req = ctx.raw.req;
+            priv_opt = varnish_sys::VRT_priv_task_get(
+                fake_ctx.ctx().raw,
+                PRIV_ANCHOR.as_ptr() as *const c_void,
+            )
+            .as_mut()
+        }
+
+        if let Some(privp) = priv_opt {
+            let mut vp: VPriv<ReplaceSteps> = VPriv::new(privp);
+            Ok(DeliveryReplacer {
+                steps: match vp.take() {
+                    Some(mut rs) => std::mem::take(&mut rs.v),
+                    None => Vec::new(),
+                },
+                body: Vec::new(),
+            })
+        } else {
+            return Ok(Default::default());
+        }
+    }
+
+    fn bytes(&mut self, ctx: &mut OutCtx, act: OutAction, buf: &[u8]) -> OutResult {
+        self.body.extend_from_slice(buf);
+
+        if let OutAction::End = act {
+            // if it's not a proper string, bailout
+            let str_body = match from_utf8(self.body.as_slice()) {
+                Err(_) => {
+                    // TODO: log error
+                    return ctx.push_bytes(act, self.body.as_slice());
+                }
+                Ok(s) => s,
+            };
+            let mut replaced_body = Cow::from(str_body);
+            for (re, sub) in &self.steps {
+                // ignore the case where the resulting `String` is `Cow::Borrowed`, it means
+                // nothing changed
+                if let Cow::Owned(s) = re.replace(&replaced_body, sub) {
+                    replaced_body = Cow::from(s);
+                }
+            }
+            ctx.push_bytes(act, replaced_body.as_bytes())
+        } else {
+            OutResult::Continue
+        }
+    }
+
+    fn name() -> &'static str {
+        "rers\0"
+    }
+}
+
+pub unsafe fn event(
+    ctx: &mut Ctx,
+    vp: &mut VPriv<varnish_sys::vdp>,
+    event: Event,
+) -> Result<(), &'static str> {
+    match event {
+        Event::Load => {
+            vp.store(new_vdp::<DeliveryReplacer>());
+            varnish_sys::VRT_AddVDP(ctx.raw, vp.as_ref().unwrap())
+        }
+        Event::Discard => varnish_sys::VRT_RemoveVDP(ctx.raw, vp.as_ref().unwrap()),
+        _ => (),
+    }
+    Ok(())
 }
